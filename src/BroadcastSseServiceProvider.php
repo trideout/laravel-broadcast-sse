b09eee46546b60d2676ed3421b94e37cbad83a23
Thomas Rideout <tom@latouraineinc.com>
2020-01-24 16:00:41 -0700

    outline

---
+++ src/BroadcastSseServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace trideout;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+use trideout\Broadcaster\SseBroadcaster;
+
+class BroadcastSseServiceProvider extends ServiceProvider implements DeferrableProvider {
+    public function register() {
+
+    }
+
+    public function provides() : array {
+        return [
+            SseBroadcaster::class,
+        ];
+    }
+}
---
+++ src/Broadcaster/SseBroadcaster.php
@@ -0,0 +1,21 @@
+<?php
+namespace trideout\Broadcaster;
+
+use Illuminate\Broadcasting\Broadcasters\Broadcaster;
+
+class SseBroadcaster extends Broadcaster {
+    public function auth($request)
+    {
+        // TODO: Implement auth() method.
+    }
+
+    public function validAuthenticationResponse($request, $result)
+    {
+        // TODO: Implement validAuthenticationResponse() method.
+    }
+
+    public function broadcast(array $channels, $event, array $payload = [])
+    {
+        // TODO: Implement broadcast() method.
+    }
+}
